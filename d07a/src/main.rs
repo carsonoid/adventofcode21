bd236a835450e75f7ffedf5ea9bc45252f7f827b
Carson Anderson <carson.anderson@getweave.com>
2021-12-07 08:37:44 -0700

    Add day 7

---
+++ d07a/src/main.rs
@@ -0,0 +1,50 @@
+use std::env;
+use std::fs;
+use std::num;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        println!("filename arg is required");
+        return;
+    }
+
+    let filename = &args[1];
+
+    println!("Reading file: {}", filename);
+
+    let contents = fs::read_to_string(filename).expect("error reading file");
+
+    let input: Vec<u64> = contents
+        .trim()
+        .split(",")
+        .map(|s| s.parse().unwrap())
+        .collect();
+
+    let max = input.iter().max().unwrap();
+    let min = input.iter().min().unwrap();
+    println!("Checking {:?}, {}, {}", input, min, max);
+
+    let mut answer_min = 0;
+    let mut answer = 0;
+    for n in *min..*max + 1 {
+        let mut sum = 0;
+        println!("Checking {}", n);
+        for crab in &input {
+            if crab < &n {
+                sum += n - crab;
+            } else {
+                sum += crab - n;
+            }
+        }
+
+        println!("  fuel = {}", sum);
+        if answer_min == 0 || sum < answer_min {
+            answer_min = sum;
+            answer = n;
+        }
+    }
+
+    println!("Best Position: {} with fuel {}", answer, answer_min)
+}
